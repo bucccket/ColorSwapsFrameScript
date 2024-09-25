b287237210ad11d9c0e4f43bd1ed9979d50bad6b
bucccket <nicolas.lisgaras@gmail.com>
2024-09-25 17:23:54 +0200

    added nix development flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Simple Python Flake";
+
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+      devShells.${system}.default  =
+      pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            python311
+            pip
+            python
+            python311Packages.pillow
+            python311Packages.xmltodict
+            jpexs
+          ];
+        };
+  };
+}
+
